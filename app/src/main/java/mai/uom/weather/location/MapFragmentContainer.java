e3ed09f4cb9372c7d401adfb38b0d36a1f009094
Despotis <panagiotis.despotis@gmail.com>
2016-01-14 19:09:30 +0200

    Basic functionality of google map (Loads the map properly )

--- app/src/main/java/mai/uom/weather/location/MapFragmentContainer.java
+++ app/src/main/java/mai/uom/weather/location/MapFragmentContainer.java
@@ -1,44 +1,55 @@
 package mai.uom.weather.location;
 
-import android.location.Location;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * MapFragmentContainerClass
  * Represents the google map with all it's functionality
  */
-public class MapFragmentContainer extends Fragment implements OnMapReadyCallback, LocationListener,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener  {
+public class MapFragmentContainer extends Fragment implements OnMapReadyCallback  {
+    /** Google map object **/
+    private GoogleMap map;
+    /** Fragment view **/
+    private View mapView;
+
 
     @Override
-    public void onConnected(Bundle bundle) {
-        
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
-    @Override
-    public void onConnectionSuspended(int i) {
 
+    /**
+     * Resister the map fragment to receive
+     * asynchronous callbacks from map object
+     */
+    private void mapAsync() {
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
-
+    @Nullable
     @Override
-    public void onLocationChanged(Location location) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /** Create the fragment view **/
+        mapView = inflater.inflate(R.layout.fragment_map_container, container, false);
+        mapAsync();
+        return mapView;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        map = googleMap;
     }
 }
