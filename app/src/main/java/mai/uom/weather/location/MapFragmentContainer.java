6ac0773bdbfac81535d3aef016452a429df92168
Despotis <panagiotis.despotis@gmail.com>
2016-01-17 17:48:58 +0200

    Renanme menu_map_container_fragment to menu_main

--- app/src/main/java/mai/uom/weather/location/MapFragmentContainer.java
+++ app/src/main/java/mai/uom/weather/location/MapFragmentContainer.java
@@ -11,9 +11,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
@@ -60,7 +57,7 @@ public class MapFragmentContainer extends Fragment implements OnMapReadyCallback
         builder.addOnConnectionFailedListener(this);
         mGoogleApiClient = builder.build();
         mGoogleApiClient.connect();
- 
+
     }
 
     /**
